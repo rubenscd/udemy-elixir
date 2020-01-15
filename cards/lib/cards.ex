1f6689031b4ac0e4ef86fd4885af2acae1ceaf06
Rubens Cavalheiro <rubens.cavalheiro@clubecandeias.com>
2020-01-15 14:16:03 -0300

    arquivos iniciais projeto cards

---
+++ cards/lib/cards.ex
@@ -0,0 +1,28 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  # def hello do
+  #   :world
+  # end
+
+  # deck de cartas
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  # embaralha o deck de cartas
+  def shuffle(deck) do
+  
+  end
+end
